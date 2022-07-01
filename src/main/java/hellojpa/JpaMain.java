a23476ee2cb69ecba787de5c31571c447c0d1eb5
jiwonAn1 <jwan0131@gmail.com>
2022-07-02 02:27:45 +0900

    영속 엔티티 동일성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Arrays;
 import java.util.List;
 
 public class JpaMain {
@@ -15,52 +16,14 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            생성.
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member);
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+            // 1차 캐시에서 값을 가져오기 때문에 캐시에 이미 있는 데이터를 조회하면 쿼리를 한 번만 실행함.
+            // 또한 영속 엔티티의 동일성을 보장함.
+            // 단, 같은 트랜잭션 안에서.
+            System.out.println("result = "+(findMember1 == findMember2)); // true.
 
-//            조회.
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getId());
-//            System.out.println("findMember = " + findMember.getName());
-
-//            삭제.
-//            Member findMember = em.find(Member.class, 1L);
-//            em.remove(findMember);
-
-//            수정.
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-
-//            JPQL 사용.
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                            .setFirstResult(1)
-//                            .setMaxResults(8)
-//                            .getResultList();
-//
-//            for(Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
-
-////////      영속성
-
-//            비영속
-//            Member member = new Member();
-//            member.setId(100L);
-//            member.setName("HelloJPA");
-
-//            영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-
-//            준영속
-//            em.detach(member);
-
-//            삭제한 상태
-//            em.remove(member);
+            // 엔티티 매니저는 데이터 변경 시 트랜잭션을 시작해야 함.
 
             tx.commit();
         } catch(Exception e) {
