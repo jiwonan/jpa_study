7b872726e7e2f4f705276851c131f8dcc8682475
jiwonAn1 <jwan0131@gmail.com>
2022-07-02 02:01:34 +0900

    JPQL 테스트.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -30,8 +31,17 @@ public class JpaMain {
 //            em.remove(findMember);
 
 //            수정.
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .setFirstResult(1)
+                            .setMaxResults(8)
+                            .getResultList();
+
+            for(Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch(Exception e) {
