e05d824eb03cc303b02163214ef0e7643596920a
jiwonAn1 <jwan0131@gmail.com>
2022-07-02 02:45:01 +0900

    flush 테스트.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,6 +17,13 @@ public class JpaMain {
         tx.begin();
         try {
             Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush(); // 쿼리가 이 시점에 호출됨.
+            // flush를 한다고 1차 캐시가 지워지지 않음.
+            // 영속성 컨텍스트에 있는 쓰기지연 SQL 저장소에 있는 것들이 DB에 반영되는 것 뿐임.
+
+            System.out.println("===================");
 
             tx.commit();
         } catch(Exception e) {
