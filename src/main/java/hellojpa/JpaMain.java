fd08b74b80765b95d5158e28596d1d1a3d0c2b70
jiwonAn1 <jwan0131@gmail.com>
2022-07-02 13:49:38 +0900

    기본 키 매핑 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,6 +16,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Member m = new Member();
+            m.setUsername("A");
+
+            em.persist(m); // identity를 사용하는 경우는 영속성을 위해 이 시점에 쿼리를 날림. (DB에 인서트하기 전에 PK값을 알 수 없기 때문.)
 
             tx.commit();
         } catch(Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,30 +6,27 @@ import java.util.Date;
 @Entity
 // @Table(name = "USER") // db 명과 클래스 명이 다를 경우 설정 가능.
 public class Member {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
     private String username;
 
-    private Integer age;
+    public Long getId() {
+        return id;
+    }
 
-    // Enum은 ORDINAL을 쓰면 안됨.
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    public String getUsername() {
+        return username;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    // 최근에는 LoalDate, LocalDateTime을 지원하기때문에 Temporal이 필요 없음.
-
-    @Lob // 큰 데이터.
-    private String description;
-
-    @Transient // DB에 컬럼 할당 x.
-    private int temp;
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public Member() {
 
